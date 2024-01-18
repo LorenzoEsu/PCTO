9717fe35059c946882bbf3555646ec739d74ce20
LorenzoEsu <157001777+LorenzoEsu@users.noreply.github.com>
2024-01-18 09:51:08 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+
+    a = 2.8;
+    b = 2.5;
+    c = 's';
+    
+    printf("a : %d\n", a);
+    printf("b : %f\n", b);
+    printf("c : %c\n", c);
+    return 0;
+}
